2deef39a59cc4f46de59f0098d33deede5c1964c
Atif khan <atifkhan161@gmail.com>
2025-09-16 16:10:30 +0530

    Remove dist folder from tracking

--- server/dist/modules/auth/auth.service.d.ts
+++
@@ -1,6 +0,0 @@
-import { DatabaseService } from '../database/database.service';
-export declare class AuthService {
-    private readonly databaseService;
-    constructor(databaseService: DatabaseService);
-    validateUser(username: string, password: string): Promise<any>;
-}
